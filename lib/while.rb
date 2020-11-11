61050b217224d0976d37fee043d502cadb908bcf
Keith Filkins <kafilkins@me.com>
2020-11-11 00:55:21 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,7 @@
-levitation_force = 6
-
-loop do
-  levitation_force += 1
-
+def using_while
+  levitation_force = 6
+  while levitation_force < 10
   puts "Wingardium Leviosa"
-
-  if levitation_force = 10
-    break
-  end
+  levitation_force += 1
+    end
 end
